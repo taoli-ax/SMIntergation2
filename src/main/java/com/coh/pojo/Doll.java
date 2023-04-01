3a5adc10d14aefa6a6a0291d8447077fd082f68a
taoli-ax <deuta-tao@outlook.com>
2023-04-01 21:35:39 +0800

    add annotation MyBean and MyResource add javaConfigForDoll add Doll add ParseAnnotation

---
+++ src/main/java/com/coh/pojo/Doll.java
@@ -0,0 +1,13 @@
+package com.coh.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Doll {
+    String name;
+    int age;
+}
